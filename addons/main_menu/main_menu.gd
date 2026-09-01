488abe1fcf527ce77610f21f00c470a29b87702f
Alex C Warren <alexcwarren.info@gmail.com>
2026-09-01 15:53:37 -0400

    Complete MainMenu

--- addons/main_menu/main_menu.gd
+++ addons/main_menu/main_menu.gd
@@ -1,10 +1,20 @@
-extends Node
+extends Control
 
 @export var start_scene: PackedScene
+
+@onready var _title_label: Label = %TitleLabel
+@onready var _start_button: Button = %StartButton
+@onready var _quit_button: Button = %QuitButton
 
 
 func _ready() -> void:
 	DebugLog.log_info(self, "ready")
+	_title_label.text = str(
+		ProjectSettings.get_setting("application/config/name", "Game Title")
+	)
+	_start_button.pressed.connect(_start_button_pressed)
+	_quit_button.pressed.connect(_quit_button_pressed)
+	_start_button.grab_focus()
 
 
 func _start_button_pressed() -> void:
